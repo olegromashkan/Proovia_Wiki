4da0670c73d948bc262349e590debddf12c28126
harry <oleg.romashkan@outlook.com>
2025-09-09 13:10:48 +0000

    уку

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "192.168.33.208", // твой локальный клиент
+    "localhost",      // стандартный доступ
+    "127.0.0.1"       // запасной
+  ],
+};
 
 export default nextConfig;
